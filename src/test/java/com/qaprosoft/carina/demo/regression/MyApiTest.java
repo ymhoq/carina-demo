a6a1fbbb39e4f72a41b3e9535c73f2393423daf9
ymhoq <fylend@gmail.com>
2022-06-20 01:02:57 +0300

    add myTest

---
+++ src/test/java/com/qaprosoft/carina/demo/regression/MyApiTest.java
@@ -0,0 +1,39 @@
+package com.qaprosoft.carina.demo.regression;
+
+import com.qaprosoft.carina.core.foundation.IAbstractTest;
+import com.qaprosoft.carina.core.foundation.api.APIMethodPoller;
+import com.qaprosoft.carina.core.foundation.utils.ownership.MethodOwner;
+import com.qaprosoft.carina.demo.api.PostUserMethod;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.Test;
+
+import java.lang.invoke.MethodHandles;
+import java.time.temporal.ChronoUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class MyApiTest implements IAbstractTest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    @Test()
+    @MethodOwner(owner = "qpsdemo")
+    public void testCreateUser() throws Exception {
+        LOGGER.info("test");
+        setCases("4555,54545");
+        PostUserMethod api = new PostUserMethod();
+        api.setProperties("api/users/user.properties");
+
+        AtomicInteger counter = new AtomicInteger(0);
+
+        api.callAPIWithRetry()
+                .withLogStrategy(APIMethodPoller.LogStrategy.ALL)
+                .peek(rs -> counter.getAndIncrement())
+                .until(rs -> counter.get() == 4)
+                .pollEvery(1, ChronoUnit.SECONDS)
+                .stopAfter(10, ChronoUnit.SECONDS)
+                .execute();
+        api.validateResponse();
+    }
+
+}
